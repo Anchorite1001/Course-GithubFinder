021d104c395515acd514010744af988830516c9a
Anchorite1001 <yangwu.1415@gmail.com>
2021-11-14 00:29:31 +1100

    single user page

--- src/components/users/UserPage.js
+++ src/components/users/UserPage.js
@@ -33,6 +33,54 @@ const UserPage = () => {
             {user.hierable 
             ? <i className='fas fa-check text-success' />
             : <i className='fas fa-times-circle text-danger' />}
+
+            <div className='card grid-2'>
+                <div className='all-center'>
+                    <img src={user.avatar_url} 
+                    className='round-img' alt=''
+                    style={{width: '150px'}}/>
+
+                    <h1>{user.name}</h1>
+                    <p>Location: {user.location}</p>
+                </div>
+                <div>
+                    {user.bio && 
+                    <Fragment>
+                        <h3>Bio</h3>
+                        <p>{user.bio}</p>
+                    </Fragment>}
+                    <a href={user.html_url}
+                    className='btn btn-dark my1'>
+                        Visit Github Profile
+                    </a>
+                    <ul>
+                        <li>
+                            {login && 
+                            <Fragment>
+                                <strong>Username: </strong> {login}
+                            </Fragment>}
+                        </li>
+                        <li>
+                            {user.company && 
+                            <Fragment>
+                                <strong>Company: </strong> {user.company}
+                            </Fragment>}
+                        </li>
+                        <li>
+                            {user.blog && 
+                            <Fragment>
+                                <strong>Website: </strong> {user.blog}
+                            </Fragment>}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className='card text-center'>
+                <div className='badge badge-primary'>Followers: {user.followers}</div>
+                <div className='badge badge-success'>Following: {user.following}</div>
+                <div className='badge badge-light'>Public Repos: {user.public_repos}</div>
+                <div className='badge badge-dark'>Public Gists: {user.public_gists}</div>
+            </div>
         </Fragment>
     )
 }
