419c42db14ab782eeae9c0de11eed3dbb937a56d
Anchorite1001 <yangwu.1415@gmail.com>
2021-11-16 19:50:14 +1100

    not found component (not working yet)

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About'
 import HomePage from './components/HomePage';
 import UserPage from './components/users/UserPage';
+import NotFound from './components/pages/NotFound';
 
 // as navbar use link and shows in every page, better put it here within the router. 
 const App = () => {
@@ -21,6 +23,7 @@ const App = () => {
                 <Route exact path='/' element={<HomePage />} />
                 <Route exact path='/about' element={<About />} />
                 <Route exact path='/user/:login' element={<UserPage />} />
+                <Route element={<NotFound />}/>
               </Routes>
           </div>
         </Router>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,30 +1,16 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
 import '../App.css';
 
 import Users from './users/Users';
 import Search from './users/Search';
 import Alert from './layout/Alert';
 
-import GithubContext from '../context/github/githubContext';
-import AlertContext from '../context/alert/alertContext';
-
 const HomePage = () => {
-  const githubContext = useContext(GithubContext);
-  const alertContext = useContext(AlertContext);
-  const [alert, setAlert] = useState(null);
-
-  // set alert for no-text search
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    setTimeout(() => setAlert(null), 5000);
-  };
-    
+  
   return (
       <div className='container'>
         <Alert/>
-        <Search
-        />
+        <Search/>
         <Users/>
       </div>
   );
